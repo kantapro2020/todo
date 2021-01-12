b83f59028c3ff6c7e978d65dc46c06cb7925da76
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2021-01-12 13:27:52 +0900

    create inMemorylogin & setting cssFolder

---
+++ src/main/java/com/todo/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder PasswordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+        http.authorizeRequests().antMatchers("/todo/src/main/resources/css/project_detail.css").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
