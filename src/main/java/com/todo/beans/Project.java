e779812a9e85ecfaf9bc55d983301ce6bda4caa6
kantapro2020 <kantapro2020@gmail.com>
2021-02-03 19:52:24 +0900

    projectList

--- src/main/java/com/todo/beans/Project.java
+++ src/main/java/com/todo/beans/Project.java
@@ -5,43 +5,42 @@ import java.sql.Date;
 
 public class Project implements Serializable {
 
-	
-	//これは何？	
-	private static final long serialVersionUID = -41353466463452342L;
+
+	private static final long serialVersionUID = 42L;
 	private int id;
 	private String project_name;
 	private int manager_id;
 	private int company_id;
 	private Date created_at;
-	
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getProject_name() {
 		return project_name;
 	}
 	public void setProject_name(String project_name) {
 		this.project_name = project_name;
 	}
-	
+
 	public int getManager_id() {
 		return manager_id;
 	}
 	public void setManager_id(int manager_id) {
 		this.manager_id = manager_id;
 	}
-	
+
 	public int getCompany_id() {
 		return company_id;
 	}
 	public void setCompany_id(int company_id) {
 		this.company_id = company_id;
 	}
-	
+
 	public Date getCreated_at() {
 		return created_at;
 	}
--- src/main/java/com/todo/repositories/ProjectRepository.java
+++ src/main/java/com/todo/repositories/ProjectRepository.java
@@ -1,5 +1,8 @@
 package com.todo.repositories;
 
+
+import java.util.LinkedList;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.todo.beans.Project;
@@ -9,4 +12,5 @@ public interface ProjectRepository {
 
 	public int registerProject(Project project);
 	public Project getProject(int id);
+	public LinkedList<Project> getProjectList(int company_id);
 }
